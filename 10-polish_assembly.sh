614aeaa00008c32a314a8cbdf1178e6f94a47f12
Samuel Mwakisha <72903872+Kishaz@users.noreply.github.com>
2024-03-02 12:40:46 +0900

    Update 10-polish_assembly.sh

--- 10-polish_assembly.sh
+++ 10-polish_assembly.sh
@@ -2,14 +2,14 @@
 
 # Replace [your_paths_here] and [your_file_names_here] with your actual paths and file names
 wkdir="path/to/Create a working directory"
-genome_fasta="genome.nextpolish-sgs-miseq.fasta"
-short_reads1="out_R1.fastq.gz"
-short_reads2="out_R2.fastq.gz"
-long_reads1="reads1.fq"
-long_reads2="reads2.fa.gz"
-hybrid_short_reads1="sf_S4_L001_R1_001.fastq"
-hybrid_short_reads2="sf_S4_L001_R2_001.fastq"
-hybrid_long_reads="JG2022-T_porechoped.fastq"
+genome_fasta="nd.asm.fasta.fasta"
+short_reads1="sf_S4_L001_R1_001.fastq"
+short_reads2="sf_S4_L001_R2_001.fastq"
+#long_reads1="reads1.fq"
+#long_reads2="reads2.fa.gz"
+#hybrid_short_reads1="reads_R1_001.fastq"
+#hybrid_short_reads2="reads_R2_001.fastq"
+#hybrid_long_reads="JG2022-T_porechoped.fastq"
 
 # Activate nextPolish Conda Environment
 conda activate nextdenovo
@@ -20,13 +20,13 @@ ls "${short_reads1}" "${short_reads2}" > sgs.fofn
 nextPolish short_reads_polishing_config.cfg
 
 # Run nextPolish for long reads only
-ls "${long_reads1}" "${long_reads2}" > lgs.fofn
-nextPolish run.cfg
+#ls "${long_reads1}" "${long_reads2}" > lgs.fofn
+#nextPolish run.cfg
 
 # Run nextPolish for short and long reads
-ls "${hybrid_short_reads1}" "${hybrid_short_reads2}" > sgs.fofn
-ls "${hybrid_long_reads}" > lgs.fofn
-nextPolish run.cfg
+#ls "${hybrid_short_reads1}" "${hybrid_short_reads2}" > sgs.fofn
+#ls "${hybrid_long_reads}" > lgs.fofn
+#nextPolish run.cfg
 
 # Deactivate nextPolish Conda Environment
 conda deactivate
