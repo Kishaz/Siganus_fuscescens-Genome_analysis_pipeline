bb9b2acfe54613fb15848523dc2ade24fc479c4a
Samuel Mwakisha <72903872+Kishaz@users.noreply.github.com>
2024-03-05 23:36:04 +0900

    Update 08-assembly_stats.sh

--- 08-assembly_stats.sh
+++ 08-assembly_stats.sh
@@ -2,8 +2,8 @@
 
 # Replace [quast_environment], [output_directory], and [input_fasta] with actual values
 quast_environment="quast5.2"
-output_directory="/path/to/your/quast_output/quast_flye_5000_2iter"
-input_fasta="/path/to/your/flye_5000_2iter/assembly.fasta"
+output_directory="/path/to/your/quast_output"
+input_fasta="/path/to/your/assembleg_genome.fasta"
 
 # Activate Conda Environment
 conda activate "${quast_environment}"
